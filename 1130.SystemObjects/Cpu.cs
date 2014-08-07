f0a2e4244a155035f177df65de71a3ae32f8bb8b
Bob Flanders <bob.flanders@gmail.com>
2014-08-07 03:02:13 -0400

    BF - Initial commit; Build load instruction.

---
+++ 1130.SystemObjects/Cpu.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1130.SystemObjects
+{
+    public enum Instructions
+    {
+        Load = 0x18
+    };
+
+    public class SystemState
+    {
+        public const int DefaultMemorySize = 32768;
+
+        public ushort[] Memory = new ushort[DefaultMemorySize];
+        public int MemorySize = DefaultMemorySize;
+        public ushort Iar;
+        public ushort Acc;
+        public ushort Ext;
+
+        public ushort Opcode;
+        public bool Format;
+        public ushort Tag;
+        public ushort Displacement;
+        public ushort Address;
+        public ushort IA;
+        public ushort Modifiers;
+
+        public void NextInstruction()
+        {
+            var firstWord = Memory[Iar];
+            Opcode = (ushort) ((firstWord & 0xF800) >> 11);
+            Format = (firstWord & 0x0400) != 0;
+            Tag = (ushort) ((firstWord & 0x0300) >> 8);
+            Iar++;
+            if (Format)
+            {
+                throw new Exception("Long form not yet implemented.");
+                // Modifiers, Address, IA, Reset displacement; iar++
+            }
+            else
+            {
+                Displacement = (ushort) (firstWord & 0xff);
+                Address = 0;
+                IA = 0;
+                Modifiers = 0;
+            }
+        }
+
+        public ushort BuildShort(Instructions instruction, uint tag, uint displacement)
+        {
+            return (ushort) ((((uint) instruction << 11) | (tag << 8) | (displacement & 0xff)) & 0xffff);
+        }
+    }
+
+    public class Cpu
+    {
+        public SystemState GetInitialSystemState()
+        {
+            var state = new SystemState();
+            return state;
+        }
+
+        public void Continue(SystemState state)
+        {
+            
+        }
+    }
+}
---
+++ Tests/UnitTests.1130.SystemObjects/CpuTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1130.SystemObjects;
+
+namespace UnitTests._1130.SystemObjects
+{
+    [TestClass]
+    public class CpuTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+    [TestClass]
+    public class SystemStateTests
+    {
+        private SystemState _state;
+
+        [TestInitialize]
+        public void BeforeEachTest()
+        {
+            _state = new SystemState {Iar = 0x100};
+        }
+
+        [TestMethod]
+        public void NextInstruction_ShortLoadInstruction()
+        {
+            _state.Memory[0x100] = _state.BuildShort(Instructions.Load, 2, 0x72);
+            _state.NextInstruction();
+            Assert.AreEqual(0x101, _state.Iar);
+            Assert.AreEqual((int) Instructions.Load, _state.Opcode);
+            Assert.AreEqual(false, _state.Format);
+            Assert.AreEqual(2, _state.Tag);
+            Assert.AreEqual(0x72, _state.Displacement);
+        }
+
+        [TestMethod]
+        public void BuildShortTest_ShortLoads()
+        {
+            Assert.AreEqual(0xc07f, _state.BuildShort(Instructions.Load, 0, 0x7f));
+            Assert.AreEqual(0xc344, _state.BuildShort(Instructions.Load, 3, 0x44));
+        }
+    }
+}
