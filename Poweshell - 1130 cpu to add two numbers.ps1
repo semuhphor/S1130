8b42d6d9d7ac35318892dcacdde617732ce4246e
BF <pair01@arpc.com>
2014-09-05 14:39:01 -0400

    BF - Added a powershell 4.0 script to invoke the 1130 cpu to add 2 numbers.

---
+++ Poweshell - 1130 cpu to add two numbers.ps1
@@ -0,0 +1,14 @@
+﻿Add-Type -Path 'C:\Development\Playground\IBM1130Emulator\1130.SystemObjects\bin\Debug\S1130.SystemObjects.dll'
+$state = New-Object S1130.SystemObjects.SystemState
+$cpu = New-Object S1130.SystemObjects.Cpu($state)
+$cpu[$cpu.Iar] = [S1130.SystemObjects.InstructionBuilder]::BuildShort([S1130.SystemObjects.Instructions.OpCodes]::Load, 0, 0x10)
+$cpu.NextInstruction()
+$cpu.Item($cpu.Iar + 0x10) = 1234
+$cpu.ExecuteInstruction()
+$cpu[$cpu.Iar] = [S1130.SystemObjects.InstructionBuilder]::BuildShort([S1130.SystemObjects.Instructions.OpCodes]::Add, 0, 0x20)
+$cpu.NextInstruction()
+$cpu.Item($cpu.Iar + 0x20) = 101
+$cpu.ExecuteInstruction()
+[string]::Format("Is it 1234 + 101 ... or {0}?", 1234 + 101)
+$cpu.Acc
+"1130 rules!"
