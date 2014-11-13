741e44abb3d24d2a742f241a8222323c7eb89c58
BF <pair01@arpc.com>
2014-11-12 22:38:58 -0500

    BF - Start of disk implementation.

--- 1130.SystemObjects/Devices/Device2501.cs
+++ 1130.SystemObjects/Devices/Device2501.cs
@@ -1,7 +1,98 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 
 namespace S1130.SystemObjects.Devices
 {
+	/* ******************************************************************************************
+	 * Device: 2310 Single Platter Disk Drive
+	 * 
+	 * This device class emulates a 2319. There are four commands accepted by the 2501: 
+	 * - Initiate read: Transfer 1 to 321 words from a specific sector of the current cylinder
+	 * - Initiate write: Tranfer 1 to 321 words to a specific sector of the current cylinder
+	 * - Control: Move the carriage to another cylinder (+/-)
+	 * - Sense device: Return the device's status word
+	 * 
+	 * The status word is as follows:
+	 *		1... .... .... ....		Data error (not implemented)
+	 *		.1.. .... .... ....		Operation complete
+	 *		..1. .... .... ....		Disk not ready (no file mounted/loaded)
+	 *		...1 .... .... ....		Disk busy (Executing a command)
+	 *		.... 1... .... ....		Carriage home (at cylinder 0)
+	 *		.... .... .... ..11		Next sector
+	 * ******************************************************************************************/
+
+	public interface ICartridge
+	{
+		bool Mounted { get; }
+		void Mount();
+	}
+
+	public class Device2310 : DeviceBase
+	{
+		public const ushort OperationComplete = 0x4000;
+		public const ushort NotReady = 0x2000;
+		public const ushort Busy = 0x1000;
+		public const ushort AtCylZero = 0x0400;
+		public const ushort NextSectorMask = 0x0003;
+
+		private readonly byte _deviceCode;
+		private ICartridge _cartridge;
+
+		public Device2310(ICpu cpu) : this(cpu, 0)
+		{
+		}
+
+		public Device2310(ICpu cpu, int driveNumber)
+		{
+			if (driveNumber < 0 || driveNumber >= 5)							// q. actual device?
+			{																	// a. no .. throw error
+				throw new ArgumentException("2310 drive number must be between 0 and 4");
+			}
+			byte[] _driveCodes = {0x4, 0x09, 0x0a, 0x0b, 0x0c};					// possible device codes ...
+			_deviceCode = _driveCodes[driveNumber];								// ... set up device code
+			CpuInstance = cpu;													// ... and save the cpu
+		}
+
+		public override byte DeviceCode
+		{
+			get { return _deviceCode; }
+		}
+
+		public override void ExecuteIocc()
+		{
+			switch (CpuInstance.IoccFunction)
+			{
+				case DevFunction.SenseDevice:
+					if ((CpuInstance.IoccModifiers & 1) == 1)
+					{
+						DeactivateInterrupt(CpuInstance);
+					}
+					CpuInstance.Acc = (ushort) (_cartridge == null ? NotReady : 0);
+//					if (_readInProgess)
+//					{
+////						CpuInstance.Acc |= BusyStatus;
+//					}
+//					if (_complete)
+//					{
+//						CpuInstance.Acc |= OperationCompleteStatus;
+//					}
+//					if (_lastCard)
+//					{
+//						CpuInstance.Acc |= LastCardStatus;
+//					}
+					break;
+				case DevFunction.InitRead:
+					break;
+			}
+		}
+
+		public void Mount(ICartridge cartridge)
+		{
+			_cartridge = cartridge;
+			_cartridge.Mount();
+		}
+	}
+
 	/* ******************************************************************************************
 	 * Device: 2501 card reader
 	 * 
---
+++ Tests/UnitTests.1130.SystemObjects/DeviceTests/Device2310Tests.cs
@@ -0,0 +1,96 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using S1130.SystemObjects.Devices;
+
+namespace UnitTests.S1130.SystemObjects.DeviceTests
+{
+	[TestClass]
+	public class Device2310Tests : DeviceTestBase
+	{
+		private Device2310 _2310;
+		private FakeCartridge _cartridge;
+
+		[TestInitialize]
+		public override void BeforeEachTest()
+		{
+			base.BeforeEachTest();
+			_2310 = new Device2310(InsCpu);
+			_cartridge = new FakeCartridge();
+		}
+
+		[TestMethod]
+		public void CreateDeviceTests_ShouldHaveCorrectDeviceId()
+		{
+			Assert.AreEqual(0x04, new Device2310(InsCpu).DeviceCode);
+			Assert.AreEqual(0x04, new Device2310(InsCpu, 0).DeviceCode);
+			Assert.AreEqual(0x09, new Device2310(InsCpu, 1).DeviceCode);
+			Assert.AreEqual(0x0a, new Device2310(InsCpu, 2).DeviceCode);
+			Assert.AreEqual(0x0b, new Device2310(InsCpu, 3).DeviceCode);
+			Assert.AreEqual(0x0c, new Device2310(InsCpu, 4).DeviceCode);
+		}
+
+		[TestMethod]
+
+		public void BadDeviceNumber_InvalidDriveNumberThrowsException()
+		{
+			TestBadDriveNumber(-1);
+			TestBadDriveNumber(5);
+		}
+
+		private void TestBadDriveNumber(int driveNumber)
+		{
+			try
+			{
+				_2310 = new Device2310(InsCpu, driveNumber);
+			}
+			catch (ArgumentException ex)
+			{
+				Assert.AreEqual("2310 drive number must be between 0 and 4", ex.Message);
+				return;
+			}
+			Assert.Fail("Bad Device number should have thrown exception.");
+		}
+
+		public void BadDeviceNumber_AboveFourThrowsException()
+		{
+			try
+			{
+				_2310 = new Device2310(InsCpu, 5);
+			}
+			catch (ArgumentException ex)
+			{
+				Assert.AreEqual("2310 drive number must be between 0 and 4", ex.Message);
+			}
+		}
+
+		[TestMethod]
+		public void WithoutDisk_DriveNotReady()
+		{
+			SenseDevice(_2310);
+			Assert.AreEqual(Device2310.NotReady, InsCpu.Acc);
+		}
+
+		[TestMethod]
+		public void WithDisk_DriveReady_MarksDriveMounted()
+		{
+			Assert.IsFalse(_cartridge.Mounted);
+			_2310.Mount(_cartridge);
+			SenseDevice(_2310);
+			Assert.AreEqual(0, InsCpu.Acc);
+			Assert.IsTrue(_cartridge.MountCalled);
+			Assert.IsTrue(_cartridge.Mounted);
+		}
+
+		public class FakeCartridge : ICartridge
+		{
+			public bool MountCalled { get; set; }
+			public bool Mounted { get; private set; }
+
+			public void Mount()
+			{
+				MountCalled = true;
+				Mounted = true;
+			}
+		}
+	}
+}
