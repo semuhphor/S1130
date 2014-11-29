409a520804bbba7696950ce0b23aa0c6111f05c9
Bob Flanders <bob.flanders@gmail.com>
2014-11-29 12:49:15 -0500

    BF - class to calculate min/max cylinder & tests

---
+++ 1130.SystemObjects/Devices/Cylinder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace S1130.SystemObjects.Devices
+{
+	/***********************************************************************
+	 * struct to calc cylinder number with floor of zero and ceiling of 202
+	 * 
+	 * Movement is limited to 202 cylinders +/-;
+	 ***********************************************************************/
+
+	public class Cylinder
+	{
+		public int Current { get; private set; }
+
+		public static Cylinder operator +(Cylinder cyl, int numberOfCylinders)
+		{
+			cyl.Current += (numberOfCylinders > 202) ? 202 : (numberOfCylinders < -202) ? -202 : numberOfCylinders;
+			if (cyl.Current < 0)
+			{
+				cyl.Current = 0;
+			}
+			if (cyl.Current > 202)
+			{
+				cyl.Current = 202;
+			}
+			return cyl;
+		}
+
+		public static Cylinder operator -(Cylinder cyl, int numberOfCylinders)
+		{
+			return cyl += (short) -numberOfCylinders;
+		}
+	}
+}
---
+++ Tests/UnitTests.1130.SystemObjects/DeviceTests/CylinderTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using S1130.SystemObjects.Devices;
+
+namespace UnitTests.S1130.SystemObjects.DeviceTests
+{
+	[TestClass]
+	public class CylinderTests
+	{
+		private Cylinder _cyl;
+
+		[TestInitialize]
+		public void BeforeEachTest()
+		{
+			_cyl = new Cylinder();	
+		}
+
+		[TestMethod]
+		public void Constructor()
+		{
+			Assert.AreEqual(0, _cyl.Current);
+		}
+
+		[TestMethod]
+		public void AddOperatorIncrements()
+		{
+			_cyl += 6;
+			Assert.AreEqual(6, _cyl.Current);
+		}
+
+		[TestMethod]
+		public void SubtractOperatorDecreaments()
+		{
+			_cyl += 7;
+			_cyl -= 3;
+			Assert.AreEqual(4, _cyl.Current);
+		}
+
+		[TestMethod]
+		public void SubtractFloorsAtZero()
+		{
+			_cyl += 7;
+			_cyl -= 10;
+			Assert.AreEqual(0, _cyl.Current);
+		}
+
+		[TestMethod]
+		public void AddCeilingAt202()
+		{
+			_cyl += 200;
+			_cyl += 25;
+			Assert.AreEqual(202, _cyl.Current);
+		}
+	}
+}
