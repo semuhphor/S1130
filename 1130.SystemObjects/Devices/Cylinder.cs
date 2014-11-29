1a658184025f7be362df3c873d2b6cbe29b3ae6c
Bob Flanders <bob.flanders@gmail.com>
2014-11-29 17:59:16 -0500

    BF - Simplified how new cylinder calculaated

--- 1130.SystemObjects/Devices/Cylinder.cs
+++ 1130.SystemObjects/Devices/Cylinder.cs
@@ -5,23 +5,24 @@ namespace S1130.SystemObjects.Devices
 	/***********************************************************************
 	 * struct to calc cylinder number with floor of zero and ceiling of 202
 	 * 
-	 * Movement is limited to 202 cylinders +/-;
+	 * Current limited to 0 - 202
 	 ***********************************************************************/
 
 	public class Cylinder
 	{
 		public int Current { get; private set; }
+		public bool Home { get { return Current == 0; } }
 
 		public static Cylinder operator +(Cylinder cyl, int numberOfCylinders)
 		{
-			cyl.Current += (numberOfCylinders > 202) ? 202 : (numberOfCylinders < -202) ? -202 : numberOfCylinders;
-			if (cyl.Current < 0)
+			var newCyl = cyl.Current + numberOfCylinders;
+			if (newCyl < 0 || newCyl > 202)
 			{
-				cyl.Current = 0;
+				cyl.Current = numberOfCylinders > 0 ? 202 : 0;
 			}
-			if (cyl.Current > 202)
+			else
 			{
-				cyl.Current = 202;
+				cyl.Current = newCyl;
 			}
 			return cyl;
 		}
