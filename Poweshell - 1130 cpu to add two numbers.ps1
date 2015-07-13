1cea664f4d2f30098df80a9693b65b68539f40d6
BF <pair01@arpc.com>
2015-07-13 16:20:28 -0400

    BF - Wanted to demo the powershell this weekend, but found it did not work because of refactoring earlier in the project. I had moved all the build instruction routines to the unit test project. Now it works as long as the Add-Types point to the correct directories.

--- Poweshell - 1130 cpu to add two numbers.ps1
+++ Poweshell - 1130 cpu to add two numbers.ps1
@@ -1,14 +1,15 @@
-﻿Add-Type -Path 'V:\Development\IBM\IBM1130\1130Emulator\1130.SystemObjects\bin\Debug\S1130.SystemObjects.dll'
-$state = New-Object S1130.SystemObjects.SystemState
-$cpu = New-Object S1130.SystemObjects.Cpu($state)
-$cpu[$cpu.Iar] = [S1130.SystemObjects.InstructionBuilder]::BuildShort([S1130.SystemObjects.Instructions.OpCodes]::Load, 0, 0x10)
+﻿Add-Type -Path 'c:\development\playground\ibm1130emulator\1130.SystemObjects\bin\Debug\S1130.SystemObjects.dll'
+Add-Type -Path 'c:\development\playground\ibm1130emulator\Tests\UnitTests.1130.SystemObjects\bin\Debug\UnitTests.S1130.SystemObjects.dll'
+$cpu = New-Object S1130.SystemObjects.Cpu
+$cpu[$cpu.Iar] = [UnitTests.S1130.SystemObjects.InstructionBuilder]::BuildShort([S1130.SystemObjects.Instructions.OpCodes]::Load, 0, 0x10)
 $cpu.NextInstruction()
-$cpu.Item($cpu.Iar + 0x10) = 1234
+$cpu.Item($cpu.Iar + 0x10) = 980
 $cpu.ExecuteInstruction()
-$cpu[$cpu.Iar] = [S1130.SystemObjects.InstructionBuilder]::BuildShort([S1130.SystemObjects.Instructions.OpCodes]::Add, 0, 0x20)
+$cpu[$cpu.Iar] = [UnitTests.S1130.SystemObjects.InstructionBuilder]::BuildShort([S1130.SystemObjects.Instructions.OpCodes]::Add, 0, 0x20)
 $cpu.NextInstruction()
-$cpu.Item($cpu.Iar + 0x20) = 101
+$cpu.Item($cpu.Iar + 0x20) = 150
 $cpu.ExecuteInstruction()
-[string]::Format("Is it 1234 + 101 ... or {0}?", 1234 + 101)
+[string]::Format("980 + 150 should add up to {0}... Does it?", 980 + 150)
 $cpu.Acc
 "1130 rules!"
+$a = Read-Host 'Hit enter to end >'
