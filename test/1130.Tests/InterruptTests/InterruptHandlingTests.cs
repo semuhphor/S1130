155d89f02508a2216833a783071752c5753c01c7
bob flanders <bob.flanders@gmail.com>
2017-01-07 23:10:28 -0500

    BF - Comment cleanup

--- test/1130.Tests/InterruptTests/InterruptHandlingTests.cs
+++ test/1130.Tests/InterruptTests/InterruptHandlingTests.cs
@@ -70,8 +70,8 @@ namespace Tests
 			Assert.NotNull(interrupt.CausingDevice.ActiveInterrupt);	// .. device still shows interrupt not complete
 			dev4.ClearActiveInterrupt();								// reset interruptint device
 			InsCpu.ClearCurrentInterrupt();								// .. now clear it (ignore BOSC... that's 1130 processing)
-			Assert.Null(InsCpu.CurrentInterruptLevel);				// .. no interrupt active
-			Assert.True(InsCpu.InterruptQueues[4].IsEmpty);			// .. no device on interrupt 4
+			Assert.Null(InsCpu.CurrentInterruptLevel);					// .. no interrupt active
+			Assert.True(InsCpu.InterruptQueues[4].IsEmpty);				// .. no device on interrupt 4
 			Assert.True(InsCpu.CurrentInterrupt.IsEmpty);				// .. and none currently active
 		}
 
@@ -82,110 +82,110 @@ namespace Tests
 			var dev4 = new DummyDevice(InsCpu);							// build first device
 			Assert.True(InsCpu.AddDevice(dev4));						// .. add to cpu
 			dev4.GetInterrupt(InsCpu, 4);								// Enqueue new interrupt
-			Assert.Equal(1, InsCpu.ActiveInterruptCount);			// .. ensure the count is correct
+			Assert.Equal(1, InsCpu.ActiveInterruptCount);				// .. ensure the count is correct
 			InsCpu.HandleInterrupt();									// handle the interrupt
 			var dev2 = new DummyDevice(InsCpu, 0x1e);					// build first device, different address
 			Assert.True(InsCpu.AddDevice(dev2));						// .. add to cpu
 			dev2.GetInterrupt(InsCpu, 2);								// Enqueue new interrupt
-			Assert.Equal(2, InsCpu.ActiveInterruptCount);			// .. ensure the count is correct
+			Assert.Equal(2, InsCpu.ActiveInterruptCount);				// .. ensure the count is correct
 			InsCpu.HandleInterrupt();									// handle the interrupt
-			Assert.Equal(2, InsCpu.ActiveInterruptCount);			// .. ensure the count is correct
-			Assert.Equal(2, InsCpu.CurrentInterrupt.Count);			// .. and both on interrupt stack
+			Assert.Equal(2, InsCpu.ActiveInterruptCount);				// .. ensure the count is correct
+			Assert.Equal(2, InsCpu.CurrentInterrupt.Count);				// .. and both on interrupt stack
 			dev2.ClearActiveInterrupt();								// reset int 2 device (like XIO SENSE DEVICE)
-			Assert.Equal(2, InsCpu.ActiveInterruptCount);			// .. ensure the count is correct
+			Assert.Equal(2, InsCpu.ActiveInterruptCount);				// .. ensure the count is correct
 			InsCpu.ClearCurrentInterrupt();								// ... should clear level 2
-			Assert.Equal(1, InsCpu.ActiveInterruptCount);			// .. ensure the count is correct
-			Assert.Equal(1, InsCpu.CurrentInterrupt.Count);			// .. and only one currently active
-			Assert.Equal(4, InsCpu.CurrentInterruptLevel);			// .. level 4 still active
-			Assert.True(InsCpu.InterruptQueues[2].IsEmpty);			// .. no device on interrupt 2
+			Assert.Equal(1, InsCpu.ActiveInterruptCount);				// .. ensure the count is correct
+			Assert.Equal(1, InsCpu.CurrentInterrupt.Count);				// .. and only one currently active
+			Assert.Equal(4, InsCpu.CurrentInterruptLevel);				// .. level 4 still active
+			Assert.True(InsCpu.InterruptQueues[2].IsEmpty);				// .. no device on interrupt 2
 			dev4.ClearActiveInterrupt();								// reset int 4 device ...
 			InsCpu.ClearCurrentInterrupt();								// .. now clear level 4 (ignore BOSC... that's 1130 processing)
-			Assert.Equal(0, InsCpu.ActiveInterruptCount);			// .. ensure the count is correct
-			Assert.Null(InsCpu.CurrentInterruptLevel);				// .. no interrupt active
-			Assert.True(InsCpu.InterruptQueues[4].IsEmpty);			// .. no device on interrupt 4
+			Assert.Equal(0, InsCpu.ActiveInterruptCount);				// .. ensure the count is correct
+			Assert.Null(InsCpu.CurrentInterruptLevel);					// .. no interrupt active
+			Assert.True(InsCpu.InterruptQueues[4].IsEmpty);				// .. no device on interrupt 4
 			Assert.True(InsCpu.CurrentInterrupt.IsEmpty);				// .. and none currently active
-			Assert.Equal(2, InsCpu.IntPool.Count);					// .. and two ints should be in the bag
+			Assert.Equal(2, InsCpu.IntPool.Count);						// .. and two ints should be in the bag
 		}
 
 		[Fact]
 		public void TestFullInterruptRoutine()
 		{
 			BeforeEachTest();
-			var dummyDevice = new DummyDevice(InsCpu);					// device causing interrupt
-			InsCpu.AddDevice(dummyDevice);								// .. add the device to the system.
+			var dummyDevice = new DummyDevice(InsCpu);																// device causing interrupt
+			InsCpu.AddDevice(dummyDevice);																			// .. add the device to the system.
 			InstructionBuilder.BuildIoccAt(dummyDevice, DevFunction.SenseDevice, 1, 1, InsCpu, 0x400);				// iocc to sense & reset the dummy device
 			InsCpu[Constants.InterruptVectors[4]] = 0x500;															// set the interrupt vector
 			InstructionBuilder.BuildShortAtAddress(OpCodes.ShiftLeft, 0, 0, InsCpu, 0x100);							// build a NOOP instruction
 			InstructionBuilder.BuildShortAtAddress(OpCodes.Wait, 0, 0, InsCpu, 0x101);								// .. then wait
 			InstructionBuilder.BuildLongAtAddress(OpCodes.ExecuteInputOuput, 0, 0x400, InsCpu, 0x501);				// XIO to reset device
 			InstructionBuilder.BuildLongIndirectBranchAtAddress(OpCodes.BranchSkip, 0, 0x40, 0x500, InsCpu, 0x503); // .. and the return from interrupt
-			dummyDevice.GetInterrupt(InsCpu, 4);						// start the interrupt
-			Assert.Equal(0x100, InsCpu.Iar);							// not in routine
-			ExecuteOneInstruction();									// execute first noop...
-			Assert.Equal(0x501, InsCpu.Iar);							// .. should be in the routine 
-			Assert.NotNull(dummyDevice.ActiveInterrupt);				// .. interrupt should not be complete
-			var interrupt = dummyDevice.ActiveInterrupt;				// .. (save the interrupt)
-			Assert.False(dummyDevice.ActiveInterrupt.InBag);			// .. should not be pooled
-			Assert.Equal(InsCpu[0x500], 0x101);						// .. return should be stored now
-			ExecuteOneInstruction();									// execute handler XIO ...
-			Assert.Equal(0x503, InsCpu.Iar);							// .. should still be in the routine 
-			Assert.Equal(0x001f, InsCpu.Acc);						// .. value returned by dummy device
-			Assert.Null(dummyDevice.ActiveInterrupt);					// .. interrupt should now be complete
-			Assert.False(interrupt.InBag);							// .. and interrupt not yet returned to the bag
-			ExecuteOneInstruction();									// return from interrupt
-			Assert.True(interrupt.InBag);								// .. and interrupt now returned to the bag
-			Assert.Equal(0x101, InsCpu.Iar);							// .. should have returned from routine
-			Assert.Null(InsCpu.CurrentInterruptLevel);				// .. no interrupt active
-			Assert.True(InsCpu.InterruptQueues[4].IsEmpty);			// .. no device on interrupt 4
-			Assert.True(InsCpu.CurrentInterrupt.IsEmpty);				// .. and none currently active
-			ExecuteOneInstruction();									// return from interrupt
-			Assert.Equal(0x102, InsCpu.Iar);							// .. should have executed wait
-			Assert.True(InsCpu.Wait);									// .. and in wait state
+			dummyDevice.GetInterrupt(InsCpu, 4);																	// start the interrupt
+			Assert.Equal(0x100, InsCpu.Iar);																		// not in routine
+			ExecuteOneInstruction();																				// execute first noop...
+			Assert.Equal(0x501, InsCpu.Iar);																		// .. should be in the routine 
+			Assert.NotNull(dummyDevice.ActiveInterrupt);															// .. interrupt should not be complete
+			var interrupt = dummyDevice.ActiveInterrupt;															// .. (save the interrupt)
+			Assert.False(dummyDevice.ActiveInterrupt.InBag);														// .. should not be pooled
+			Assert.Equal(InsCpu[0x500], 0x101);																		// .. return should be stored now
+			ExecuteOneInstruction();																				// execute handler XIO ...
+			Assert.Equal(0x503, InsCpu.Iar);																		// .. should still be in the routine 
+			Assert.Equal(0x001f, InsCpu.Acc);																		// .. value returned by dummy device
+			Assert.Null(dummyDevice.ActiveInterrupt);																// .. interrupt should now be complete
+			Assert.False(interrupt.InBag);																			// .. and interrupt not yet returned to the bag
+			ExecuteOneInstruction();																				// return from interrupt
+			Assert.True(interrupt.InBag);																			// .. and interrupt now returned to the bag
+			Assert.Equal(0x101, InsCpu.Iar);																		// .. should have returned from routine
+			Assert.Null(InsCpu.CurrentInterruptLevel);																// .. no interrupt active
+			Assert.True(InsCpu.InterruptQueues[4].IsEmpty);															// .. no device on interrupt 4
+			Assert.True(InsCpu.CurrentInterrupt.IsEmpty);															// .. and none currently active
+			ExecuteOneInstruction();																				// return from interrupt
+			Assert.Equal(0x102, InsCpu.Iar);																		// .. should have executed wait
+			Assert.True(InsCpu.Wait);																				// .. and in wait state
 		}
 
 		[Fact]
 		public void TestIfInterruptNotReset()
 		{
 			BeforeEachTest();
-			var dummyDevice = new DummyDevice(InsCpu);					// device causing interrupt
-			InsCpu.AddDevice(dummyDevice);								// .. add the device to the system.
+			var dummyDevice = new DummyDevice(InsCpu);																// device causing interrupt
+			InsCpu.AddDevice(dummyDevice);																			// .. add the device to the system.
 			InstructionBuilder.BuildIoccAt(dummyDevice, DevFunction.SenseDevice, 0, 1, InsCpu, 0x400);				// iocc to sense & NOT reset the dummy device
 			InsCpu[Constants.InterruptVectors[4]] = 0x500;															// set the interrupt vector
 			InstructionBuilder.BuildShortAtAddress(OpCodes.ShiftLeft, 0, 0, InsCpu, 0x100);							// build a NOOP instruction
 			InstructionBuilder.BuildShortAtAddress(OpCodes.Wait, 0, 0, InsCpu, 0x101);								// .. then wait
 			InstructionBuilder.BuildLongAtAddress(OpCodes.ExecuteInputOuput, 0, 0x400, InsCpu, 0x501);				// XIO to sense, NOT reset device
 			InstructionBuilder.BuildLongIndirectBranchAtAddress(OpCodes.BranchSkip, 0, 0x40, 0x500, InsCpu, 0x503); // .. and the return from interrupt
-			dummyDevice.GetInterrupt(InsCpu, 4);						// start the interrupt
-			Assert.Equal(0x100, InsCpu.Iar);							// not in routine
-			ExecuteOneInstruction();									// execute first noop...
-			Assert.Equal(0x501, InsCpu.Iar);							// .. should be in the routine 
-			Assert.NotNull(dummyDevice.ActiveInterrupt);				// .. interrupt should not be complete
-			var interrupt = dummyDevice.ActiveInterrupt;				// .. (save the interrupt)
-			Assert.False(dummyDevice.ActiveInterrupt.InBag);			// .. should not be pooled
-			Assert.Equal(InsCpu[0x500], 0x101);						// .. return should be stored now
-			ExecuteOneInstruction();									// execute handler XIO ...
-			Assert.Equal(0x503, InsCpu.Iar);							// .. should still be in the routine 
-			Assert.Equal(0x001f, InsCpu.Acc);						// .. value returned by dummy device
-			Assert.NotNull(dummyDevice.ActiveInterrupt);				// .. interrupt should not be complete
-			Assert.False(interrupt.InBag);							// .. and interrupt still out in the wild
-			ExecuteOneInstruction();									// return from interrupt
-			Assert.Equal(0x501, InsCpu.Iar);							// .. should be back in the routine
-			Assert.Equal(4, InsCpu.CurrentInterruptLevel);			// .. interrupt active
-			Assert.False(InsCpu.InterruptQueues[4].IsEmpty);			// .. no device on interrupt 4
-			Assert.False(InsCpu.CurrentInterrupt.IsEmpty);			// .. and none currently active
-			ExecuteOneInstruction();									// sense device .. no reset
-			ExecuteOneInstruction();									// return from interrupt
-			Assert.Equal(0x501, InsCpu.Iar);							// .. should be back in the routine
-			Assert.Equal(1, InsCpu.ActiveInterruptCount);			// .. interrupt still active
-			ExecuteOneInstruction();									// sense device .. no reset
-			ExecuteOneInstruction();									// return from interrupt
-			Assert.Equal(0x501, InsCpu.Iar);							// .. should be back in the routine
-			Assert.Equal(1, InsCpu.ActiveInterruptCount);			// .. interrupt still active
-			InsCpu[0x401] |= 1;											// make the sense device reset the interrupt
-			ExecuteOneInstruction();									// sense device .. no reset
-			ExecuteOneInstruction();									// return from interrupt
-			Assert.Equal(0x101, InsCpu.Iar);							// .. should be back in the routine
-			Assert.Equal(0, InsCpu.ActiveInterruptCount);			// .. interrupt still active
+			dummyDevice.GetInterrupt(InsCpu, 4);																	// start the interrupt
+			Assert.Equal(0x100, InsCpu.Iar);																		// not in routine
+			ExecuteOneInstruction();																				// execute first noop...
+			Assert.Equal(0x501, InsCpu.Iar);																		// .. should be in the routine 
+			Assert.NotNull(dummyDevice.ActiveInterrupt);															// .. interrupt should not be complete
+			var interrupt = dummyDevice.ActiveInterrupt;															// .. (save the interrupt)
+			Assert.False(dummyDevice.ActiveInterrupt.InBag);														// .. should not be pooled
+			Assert.Equal(InsCpu[0x500], 0x101);																		// .. return should be stored now
+			ExecuteOneInstruction();																				// execute handler XIO ...
+			Assert.Equal(0x503, InsCpu.Iar);																		// .. should still be in the routine 
+			Assert.Equal(0x001f, InsCpu.Acc);																		// .. value returned by dummy device
+			Assert.NotNull(dummyDevice.ActiveInterrupt);															// .. interrupt should not be complete
+			Assert.False(interrupt.InBag);																			// .. and interrupt still out in the wild
+			ExecuteOneInstruction();																				// return from interrupt
+			Assert.Equal(0x501, InsCpu.Iar);																		// .. should be back in the routine
+			Assert.Equal(4, InsCpu.CurrentInterruptLevel);															// .. interrupt active
+			Assert.False(InsCpu.InterruptQueues[4].IsEmpty);														// .. no device on interrupt 4
+			Assert.False(InsCpu.CurrentInterrupt.IsEmpty);															// .. and none currently active
+			ExecuteOneInstruction();																				// sense device .. no reset
+			ExecuteOneInstruction();																				// return from interrupt
+			Assert.Equal(0x501, InsCpu.Iar);																		// .. should be back in the routine
+			Assert.Equal(1, InsCpu.ActiveInterruptCount);															// .. interrupt still active
+			ExecuteOneInstruction();																				// sense device .. no reset
+			ExecuteOneInstruction();																				// return from interrupt
+			Assert.Equal(0x501, InsCpu.Iar);																		// .. should be back in the routine
+			Assert.Equal(1, InsCpu.ActiveInterruptCount);															// .. interrupt still active
+			InsCpu[0x401] |= 1;																						// make the sense device reset the interrupt
+			ExecuteOneInstruction();																				// sense device .. no reset
+			ExecuteOneInstruction();																				// return from interrupt
+			Assert.Equal(0x101, InsCpu.Iar);																		// .. should be back in the routine
+			Assert.Equal(0, InsCpu.ActiveInterruptCount);															// .. interrupt still active
 		}
 	}
 }
