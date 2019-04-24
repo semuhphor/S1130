fd9690f3763b2b85196f9b122b11063420b49151
Bob Flanders <bob.flanders@gmail.com>
2019-04-23 21:54:09 -0400

    BF - Updated powershell to work again

--- Poweshell - 1130 cpu to add two numbers.ps1
+++ Poweshell - 1130 cpu to add two numbers.ps1
@@ -1,11 +1,11 @@
-﻿Add-Type -Path 'c:\development\playground\ibm1130emulator\1130.SystemObjects\bin\Debug\S1130.SystemObjects.dll'
-Add-Type -Path 'c:\development\playground\ibm1130emulator\Tests\UnitTests.1130.SystemObjects\bin\Debug\UnitTests.S1130.SystemObjects.dll'
+﻿Add-Type -Path '.\src\S1130.SystemObjects\bin\Debug\netcoreapp2.2\S1130.SystemObjects.dll'
+Add-Type -Path '.\test\UnitTests.S1130\bin\Debug\netcoreapp2.2\UnitTests.S1130.dll'
 $cpu = New-Object S1130.SystemObjects.Cpu
-$cpu[$cpu.Iar] = [UnitTests.S1130.SystemObjects.InstructionBuilder]::BuildShort([S1130.SystemObjects.Instructions.OpCodes]::Load, 0, 0x10)
+$cpu[$cpu.Iar] = [UnitTests.S1130.InstructionBuilder]::BuildShort([S1130.SystemObjects.Instructions.OpCodes]::Load, 0, 0x10)
 $cpu.NextInstruction()
 $cpu.Item($cpu.Iar + 0x10) = 980
 $cpu.ExecuteInstruction()
-$cpu[$cpu.Iar] = [UnitTests.S1130.SystemObjects.InstructionBuilder]::BuildShort([S1130.SystemObjects.Instructions.OpCodes]::Add, 0, 0x20)
+$cpu[$cpu.Iar] = [UnitTests.S1130.InstructionBuilder]::BuildShort([S1130.SystemObjects.Instructions.OpCodes]::Add, 0, 0x20)
 $cpu.NextInstruction()
 $cpu.Item($cpu.Iar + 0x20) = 150
 $cpu.ExecuteInstruction()
